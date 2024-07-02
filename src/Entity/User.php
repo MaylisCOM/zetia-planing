4929a25b8a5a1d11b5bc8185bb89c850f5e68002
Maylis <maylis.pala.pro@gmail.com>
2024-07-02 18:14:51 +0200

    bug login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,11 +8,12 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
-    #[ORM\Table(name: '`user`')]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
